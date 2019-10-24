421771a18b8e1441793d26b6c668286765090b81
Farhad Safarov <farhad.safarov@gmail.com>
2019-10-24 20:40:26 +0300

    analyser test

---
+++ tests/Unit/AnalyserTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Seferov\Typhp\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Seferov\Typhp\Analyser;
+use Seferov\Typhp\Issue\IssueInterface;
+use Seferov\Typhp\IssueCollection;
+
+class AnalyserTest extends TestCase
+{
+    public function testAnalyse()
+    {
+        $code = '<?php
+            function foo($bar) {
+                return $bar > 0;
+            }
+            
+            $foo = function ($foo) {
+                return $foo;
+            };
+            
+            array_map($foo, [0, 1, 2]);
+            
+            class ClosuresWithIssues
+            {
+                public function foo($foo)
+                {
+                    $foo(1);
+                }
+
+                /**
+                 * {@inheritDoc}
+                 */
+                public function inheritdoc()
+                {
+                    return $this->foo();
+                }
+            
+                /**
+                 * @return object|array
+                 */
+                public function multiple()
+                {
+                    return $this->foo();
+                }
+            }
+            
+            (new ClosuresWithIssues())->foo(function ($bar) {
+                return $bar > 1;
+            });
+        ';
+
+        $analyser = new Analyser($code);
+        $issueCollection = $analyser->analyse();
+        $this->assertInstanceOf(IssueCollection::class, $issueCollection);
+        $this->assertCount(8, $issueCollection);
+
+        $this->assertInstanceOf(IssueInterface::class, $issueCollection->current());
+    }
+}
